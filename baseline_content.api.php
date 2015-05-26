06094480fd0acc9fd20f72f32acc3076eb7be43b
Algeron Waterschoot <algeron.waterschoot@gmail.com>
2015-05-26 14:12:43 +0200

    Add support for Panels & Node Export

---
+++ baseline_content.api.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Documentation for hooks defined by Baseline Content.
+ */
+
+/**
+ * Registers your module as an implementor of Baseline Content functionality.
+ */
+function hook_baseline_content_api() {
+  return array();
+}
